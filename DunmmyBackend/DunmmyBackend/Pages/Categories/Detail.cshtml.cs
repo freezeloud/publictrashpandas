c44b4d42bed8d6cee701e28f7e684a79a0f9ad01
Juřička Tomáš <tomas.juricka@outlook.cz>
2020-12-05 11:23:38 +0100

    Move categories page, add detail page, connect pages

---
+++ DunmmyBackend/DunmmyBackend/Pages/Categories/Detail.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
+
+namespace DunmmyBackend.Pages.Categories
+{
+    public class DetailModel 
+    {
+
+    }
+}
