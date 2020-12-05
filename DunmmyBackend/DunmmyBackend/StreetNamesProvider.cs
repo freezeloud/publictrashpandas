85cca7c528d8817cb7f23c72fc9c3443c98cc9e6
Šindelář Jiří <sindelar@cngroup.dk>
2020-12-05 08:55:44 +0100

    Dummy data generation

---
+++ DunmmyBackend/DunmmyBackend/StreetNamesProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DunmmyBackend
+{
+    public class StreetNamesProvider : IStreetNamesProvider
+    {
+        private List<string> StreetNames => _streetNames ?? GetStreetNamesAsync().GetAwaiter().GetResult();
+        private List<string> _streetNames;
+        private int _streetNamesCount;
+
+        async Task<List<string>> GetStreetNamesAsync()
+        {
+            _streetNames = (await File.ReadAllLinesAsync(@"Artifacts/ulice.csv", Encoding.Latin1)).ToList();
+            _streetNamesCount = _streetNames.Count;
+            return _streetNames;
+        }
+
+        public IEnumerable<string> GetRandomStreetNames()
+        {
+            var rand = new Random(DateTime.Now.Millisecond);
+            yield return StreetNames[rand.Next(0, _streetNamesCount)];
+        }
+    }
+
+    public interface IStreetNamesProvider
+    {
+        IEnumerable<string> GetRandomStreetNames();
+    }
+}
